7781ef545e4d8759d28be421a03af6654bd9aa5d
Jacob <henriksen.jacob@gmail.com>
2025-07-04 00:13:51 +0200

    Fix import path for units JSON file in unitService.js

--- js/services/unitService.js
+++ js/services/unitService.js
@@ -1,5 +1,5 @@
 // js/unitService.js
-import units from '../data/units.json' with { type: 'json' };
+import units from '../../data/units.json' with { type: 'json' };
 
 export function getUnitByName(name) {
   const allUnits = [...units.vampires, ...units.humans];
